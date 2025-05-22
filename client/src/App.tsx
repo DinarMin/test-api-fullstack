44171e4a3babb313ca729673da0f19b31ddbc74b
Dinar2202 <dinar2202@gmail.com>
2025-05-22 21:05:31 +0300

    fix: API in APP.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import type { DropResult } from "react-beautiful-dnd";
 
 import "./App.css";
 
-const API = "http://localhost:3000";
+const API = "https://test-api-fullstack.onrender.com";
 const LIMIT = 20;
 
 const App: React.FC = () => {
